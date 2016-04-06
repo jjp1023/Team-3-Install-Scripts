7ce919523c37d4d9f91edfdc9a34c8cc66bae60d
thegeekkid <brian@geekkidconsulting.com>
2016-04-06 12:06:56 -0500

    Finished first draft of jenkins deployment script.

--- Jenkins/deployment.sh
+++ Jenkins/deployment.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#------------------------------------------Setup bash script-------------------------------------------------
+function pause(){
+   read -p "$*"
+}
+#----------------------------------------End Setup bash script-----------------------------------------------
+
 #---------------------------Base System Install - should be run on every system------------------------------
 apt-get update
 apt-get install -y curl
@@ -25,4 +31,40 @@ sudo apt-get update
 sudo apt-get install jenkins
 #----------------------------------------Finish Install Jenkins----------------------------------------------
 
-#TODO: Configure Jenkins
+#------------------------------------------Configure System--------------------------------------------------
+echo alias confj="cd /var/lib/jenkins">>~/.zshrc
+echo clear>>~/.zshrc
+echo echo "You Are Doomed OS (C) 2016 - Brian Semrau">>~/.zshrc
+ssh-keygen -t rsa -b 4096 -C "brian@geekkidconsulting.com" -f /root/.ssh/gh_rsa -N ""
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/gh_rsa
+pause 'Press [Enter] key to continue...'
+clear
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo +                                                                              +
+echo +  Congratulations oh system administrator!  You have successfully advanced    +
+echo +  to level of "script kiddie".  To advance to the next level, you will to     +
+echo +  copy the output below to the deploy keys (read/write) of the scripts repo.  +
+echo +  (https://github.com/ITMT-430/Team-3-Install-Scripts).                       +
+echo +                                                                              +
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo -e Output to copy:/n/n
+cat ~/.ssh/gh_rsa.pub
+echo -e /n/n/n To continue, press [Enter].
+pause ''
+#--------------------------------------Finish Configure System-----------------------------------------------
+
+#----------------------------------------Configure Jenkins---------------------------------------------------
+service jenkins stop
+cd /var/lib/jenkins
+rm -rf /var/lib/jenkins/*
+git clone git@github.com:ITMT-430/team-3-jenkins-config.git
+service jenkins start
+#---------------------------------------End Configure Jenkins------------------------------------------------
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo +                                                                              +
+echo +  Congratulations oh system administrator!  You have successfully completed   +
+echo +  your script kiddie quest and installed and configured Jenkins.  Good bye!   +
+echo +  sudo rm -rf /                                                               +
+echo +                                                                              +
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
