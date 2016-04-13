9f6315dd74943ca720d48282383e75691dafb76b
thegeekkid <brian@geekkidconsulting.com>
2016-04-13 16:38:03 -0500

    Fixed euca2ools git repo location.

--- deploy-environment.sh
+++ deploy-environment.sh
@@ -5,9 +5,8 @@
 apt-get update
 apt-get install -y git
 apt-get install -y python-setuptools python-dev libxslt1-dev libxml2 libxml2-dev zlib1g-dev
-mkdir /euca2ools
+git clone https://github.com/jhajek/euca2ools.git /euca2ools
 cd /euca2ools
-git clone https://github.com/jhajek/euca2ools.git
 git checkout origin/maint-3.1
 python setup.py install
 read -p "Verify there are no errors up to this point, then press any key to continue." nothing
