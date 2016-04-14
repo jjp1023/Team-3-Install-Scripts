0f4a4aef6d332032d76a77c2f0e76a96604c4c47
thegeekkid <brian@geekkidconsulting.com>
2016-04-13 19:04:35 -0500

    Added the ability to get the instance ID and IP address to a var.

--- deploy-environment.sh
+++ deploy-environment.sh
@@ -17,3 +17,9 @@ unzip -d creds creds.zip
 source /euca2ools/creds/eucarc
 echo "source /euca2ools/creds/eucarc">>~/.bashrc
 echo "source /euca2ools/creds/eucarc">>~/.zshrc
+echo "Sometimes one of these commands doesn't work right in a script."
+echo "If there was an error above, please open a new terminal and type:"
+read -p "source /euca2ools/creds/eucarc" nothing
+euca-version
+instance="$(euca-run-instances emi-c87b2863 -n 1 -k team3-new -g 'Team 3 Jenkins' -t c1.xlarge | grep -o 'i-.\{0,8\}' | head -1)"
+ipad=$(euca-describe-instances | grep ${instance} | grep -o '64\.131\.111\..\{0,3\}' | tr -s [:space:])
