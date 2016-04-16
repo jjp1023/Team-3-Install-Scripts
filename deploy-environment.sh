e9a7624885a13d7de5a602209f9c6b60dcb2f169
thegeekkid <brian@geekkidconsulting.com>
2016-04-15 20:20:23 -0500

    Added the config part of script and address allocation abilities.

--- deploy-environment.sh
+++ deploy-environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: Run on vagrant machine as root (not sudo - actually switch to root):
+# Usage: Run on vagrant machine or another Debian box as root (not sudo - actually switch to root):
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ITMT-430/Team-3-Install-Scripts/master/deploy-environment.sh)"
 apt-get update
 apt-get install -y git
@@ -23,3 +23,30 @@ read -p "source /euca2ools/creds/eucarc" nothing
 euca-version
 instance="$(euca-run-instances emi-c87b2863 -n 1 -k team3-new -g 'Team 3 Jenkins' -t c1.xlarge | grep -o 'i-.\{0,8\}' | head -1)"
 ipad=$(euca-describe-instances | grep ${instance} | grep -o '64\.131\.111\..\{0,3\}' | tr -s [:space:])
+read -p 'Please open a new window and ssh into ${ipad} and verify the connection works.' nothing
+confirm="n"
+while [![${confirm}=="Y"]]; do
+  read -p "Are you ready to continue the script? (Y/n)" confirm
+done
+ssh root@${ipad} 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ITMT-430/Team-3-Install-Scripts/master/Jenkins/deployment.sh)"'
+cont="True"
+read -p "Did the script complete successfully? (Y/n)" confirm
+if [ ![ ${confirm}=="Y") ] ]
+then
+  read -p "Are you sure? (Y/n)" confirm
+  if [ ![ ${confirm}=="Y"] ]
+  then
+    cont="False"
+    euca-terminate-instances ${instance}
+    echo Euca instance terminated due to script error.
+    echo Please attempt running again, if it still fails,
+    echo please troubleshoot the script.
+    exit
+  fi
+fi
+if [ cont=="True" ]
+then
+  euca-associate-address -i ${instance} -a 64.131.111.60
+  read -p "Proper address assigned.  Jenkins should be up and running."
+fi
+exit
