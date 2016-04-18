c67f3b80b416723d7d8fb765eaa53e39d785c5a2
thegeekkid <brian@geekkidconsulting.com>
2016-04-18 10:34:31 -0500

    Added correct way to get the info into vars.

--- deploy-environment.sh
+++ deploy-environment.sh
@@ -21,7 +21,8 @@ echo "Sometimes one of these commands doesn't work right in a script."
 echo "If there was an error above, please open a new terminal and type:"
 read -p "source /euca2ools/creds/eucarc" nothing
 euca-version
-instance="$(euca-run-instances emi-c87b2863 -n 1 -k team3-new -g 'Team 3 Jenkins' -t c1.xlarge | grep -o 'i-.\{0,8\}' | head -1)"
+output="$(euca-run-instances emi-c87b2863 -n 1 -k team3-new -g 'Team 3 Jenkins' -t c1.xlarge)"
+instance=$(echo "${output}" | grep -o 'i-.\{0,8\}' | head -1)
 ipad=$(euca-describe-instances | grep ${instance} | grep -o '64\.131\.111\..\{0,3\}' | tr -s [:space:])
 read -p 'Please open a new window and ssh into ${ipad} and verify the connection works.' nothing
 confirm="n"
