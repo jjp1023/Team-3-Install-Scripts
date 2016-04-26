6ec36dd3e7452b2e8c5a5202746cfdd584b9729e
thegeekkid <brian@geekkidconsulting.com>
2016-04-26 15:18:44 -0500

    Created menu for script.

--- deploy-environment.sh
+++ deploy-environment.sh
@@ -23,44 +23,81 @@ then
   touch /bs/eucainstall
 fi
 
+mainmenu
 
-euca-version
-output="$(euca-run-instances emi-c87b2863 -n 1 -k team3-new -g 'Team 3 Jenkins' -t cc1.4xlarge)"
-echo ""
-echo '****************************************'
-echo '*                                      *'
-echo '*      Sleeping for 20 seconds.        *'
-echo '*       Please freaking wait.          *'
-echo '*                                      *'
-echo '****************************************'
-sleep 20
-echo ""
-instance="$(echo "${output}" | grep -o 'i-.\{0,8\}' | head -1)"
-ipad="$(euca-describe-instances | grep ${instance} | grep -o '64\.131\.111\..\{0,3\}' | tr -s [:space:])"
-read -p "Please open a new window and ssh into ${ipad} and verify the connection works." nothing
-confirm="n"
-while [![ ${confirm} = "Y" ]]; do
+function mainmenu {
+  clear
+  euca-version
+  echo ""
+  echo '****************************************'
+  echo '*                                      *'
+  echo '*             Main Menu                *'
+  echo '*  What would you like to install/do?  *'
+  echo '*                                      *'
+  echo '****************************************'
+  OPTIONS="Jenkins Production 'Full Environment' Quit"
+  select opt in $OPTIONS; do
+    if [ "$opt" = "Jenkins" ]; then
+      jankie
+    elif [ "$opt" = "Production" ]; then
+      production
+    elif [ "$opt" = "Full Environment" ]; then
+      everything
+    elif [ "$opt" = "Quit" ]; then
+      exit
+    fi
+  done
+}
+
+
+
+function jankie {
+  output="$(euca-run-instances emi-c87b2863 -n 1 -k team3-new -g 'Team 3 Jenkins' -t cc1.4xlarge)"
+  echo ""
+  echo '****************************************'
+  echo '*                                      *'
+  echo '*      Sleeping for 20 seconds.        *'
+  echo '*       Please freaking wait.          *'
+  echo '*                                      *'
+  echo '****************************************'
+  sleep 20
+  echo ""
+  instance="$(echo "${output}" | grep -o 'i-.\{0,8\}' | head -1)"
+  ipad="$(euca-describe-instances | grep ${instance} | grep -o '64\.131\.111\..\{0,3\}' | tr -s [:space:])"
+  read -p "Please open a new window and ssh into ${ipad} and verify the connection works." nothing
+  confirm="n"
+  while [![ ${confirm} = "Y" ]]; do
   read -p "Are you ready to continue the script? (Y/n)" confirm
-done
-ssh root@${ipad} 'bash <(curl -s https://raw.githubusercontent.com/ITMT-430/Team-3-Install-Scripts/master/Jenkins/deployment.sh)'
-cont="True"
-read -p "Did the script complete successfully? (Y/n)" confirm
-if [![ ${confirm} = "Y" ]];
-then
-  read -p "Are you sure? (Y/n)" confirm
+  done
+  ssh root@${ipad} 'bash <(curl -s https://raw.githubusercontent.com/ITMT-430/Team-3-Install-Scripts/master/Jenkins/deployment.sh)'
+  cont="True"
+  read -p "Did the script complete successfully? (Y/n)" confirm
   if [![ ${confirm} = "Y" ]];
   then
-    cont="False"
-    euca-terminate-instances ${instance}
-    echo Euca instance terminated due to script error.
-    echo Please attempt running again, if it still fails,
-    echo please troubleshoot the script.
-    exit
+    read -p "Are you sure? (Y/n)" confirm
+    if [![ ${confirm} = "Y" ]];
+    then
+      cont="False"
+      euca-terminate-instances ${instance}
+      echo Euca instance terminated due to script error.
+      echo Please attempt running again, if it still fails,
+      echo please troubleshoot the script.
+      exit
+    fi
   fi
-fi
-if [ ${cont} = "True" ];
-then
-  euca-associate-address -i ${instance} -a 64.131.111.60
-  read -p "Proper address assigned.  Jenkins should be up and running."
-fi
-exit
+  if [ ${cont} = "True" ];
+  then
+    euca-associate-address -i ${instance} -a 64.131.111.60
+    read -p "Proper address assigned.  Jenkins should be up and running."
+  fi
+}
+
+function production {
+  echo Production isn''t ready yet.
+  mainmenu
+}
+
+function everything {
+  echo Function ""everything"" isn''t ready yet.
+  mainmenu
+}
