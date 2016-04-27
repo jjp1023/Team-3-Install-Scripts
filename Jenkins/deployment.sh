15151f1129bcd64ae038fefcae8f533b3adafbbf
thegeekkid <brian@geekkidconsulting.com>
2016-04-27 10:51:44 -0500

    Added a copy function.

--- Jenkins/deployment.sh
+++ Jenkins/deployment.sh
@@ -74,6 +74,7 @@ rm -rf /var/lib/jenkins
 mkdir /var/lib/jenkins
 chmod 777 /var/lib/jenkins
 bash <("git clone git@github.com:ITMT-430/team-3-jenkins-config.git /root/jenkins-config/")
+cp /root/jenkins-config/* /var/lib/jenkins/
 #Jenkins always crashes without 777 permissions.  Tried a ton of things, can't figure out a way around it.  :/
 #Reset permissions after clone just in case.
 chmod 777 /var/lib/jenkins
